2edeb9dc0ef29b9648444cc20b9ba41f9d7dc523
khanhtrann <vankhanhkakki@gmail.com>
2022-06-29 11:39:18 +0700

    Maven project initial commmit

---
+++ src/main/java/com/khanh/mathutil/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.khanh.mathutil.main;
+
+import com.khanh.mathutil.core.MathUtil;
+
+/**
+ *
+ * @author vankh
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("5! = " + MathUtil.getFactorial(5));
+    }
+}
